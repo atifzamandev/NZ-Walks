00a1e5e09fb5d7106124d25baf86322040303acd
Atif Zaman <atifzamandev@gmail.com>
2025-11-13 10:30:23 +0100

    Auth controller to register a user.

---
+++ NZ Walks.API/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZ_Walks.API.Models.DTO;
+
+namespace NZ_Walks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController:ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Email,
+            };
+
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if(identityResult.Succeeded)
+            {
+                if(registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+                    if(identityResult.Succeeded)
+                    {
+                        return Ok("User registered sucessfully. Please login now.");
+                    }
+                }
+            }
+            return BadRequest("Somthing Went wrong!");
+
+        }
+
+    }
+}
---
+++ NZ Walks.API/Models/DTO/RegisterRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZ_Walks.API.Models.DTO
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string[] Roles { get; set; }
+
+    }
+}
