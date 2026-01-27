41a573656463b1801efe606e2082414e85819295
Atif Zaman <atifzamandev@gmail.com>
2026-01-27 10:47:23 +0100

    Added MVC project to consume web api

---
+++ NZWalks.UI/Controllers/RegionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.UI.Controllers
+{
+    public class RegionController:Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        public RegionController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+                var httpResponseMessage = await client.GetAsync("https://localhost:7036/api/regions");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                var stringResponseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                ViewBag.Response = stringResponseBody;
+
+            }
+            catch(Exception ex)
+            {
+            }
+
+            return View();
+        }
+    }
+}
